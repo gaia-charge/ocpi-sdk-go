50fbfa2cb4df72db25bc8bb41097eb74ecd35629
Blaž Dular <22869613+xBlaz3kx@users.noreply.github.com>
2025-03-13 20:28:47 +0100

    Feature: Client & server generation on spec change (#6)
    * feat: autogen on pr or push if validation is successful
    
    * [Autogen] Generated client and server code from OpenAPI spec

--- 2.2.1/tools.go
+++ 2.2.1/tools.go
@@ -1,4 +1,4 @@
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config ./cfg.client.yaml ./api/2.2.1.spec.yml
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config ./cfg.server.yaml ./api/2.2.1.spec.yml
+//go:generate oapi-codegen -config ./cfg.client.yaml ./api/2.2.1.spec.yml
+//go:generate oapi-codegen -config ./cfg.server.yaml ./api/2.2.1.spec.yml
 
 package ocpi_v221
